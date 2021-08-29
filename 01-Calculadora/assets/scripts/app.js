c44784fcc9b34800f06f6a84e6d113c06d7d095e
Sergio <sergiohidalgocaceres@gmail.com>
2021-08-29 18:53:05 -0500

    feat: conversion de cadena a número

--- 01-Calculadora/assets/scripts/app.js
+++ 01-Calculadora/assets/scripts/app.js
@@ -2,7 +2,10 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 
 function add() {
-  currentResult = currentResult + userInput.value;
+  // currentResult = currentResult + userInput.value;
+  // currentResult = currentResult + parseInt(userInput.value);
+  // currentResult = currentResult + parseFloat(userInput.value);
+  currentResult = currentResult + +userInput.value;
   outputResult(currentResult, '');
 }
 
