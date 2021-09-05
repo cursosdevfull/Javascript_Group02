63647f44f5c2e3a34373210fcc0869a9031cfe6f
Sergio <sergiohidalgocaceres@gmail.com>
2021-09-05 18:21:41 -0500

    ataque normal

--- 02-Mounstro/assets/scripts/app.js
+++ 02-Mounstro/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 14;
+const MONSTER_ATTACK_VALUE = 15;
 
 const choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
@@ -9,6 +10,16 @@ adjustHealthBars(choseMaxLife);
 function attackHandler() {
   const damage = dealMonsterDamage(ATTACK_VALUE);
   currentMonsterHealth -= damage;
+  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  currentPlayerHealth -= playerDamage;
+
+  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+    alert('Gané');
+  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+    alert('Perdí');
+  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+    alert('Empaté');
+  }
 }
 
 attackBtn.addEventListener('click', attackHandler);
