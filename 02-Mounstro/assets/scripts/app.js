d1cb6888b922b06c2e9b3e89a529c92df5fd65b0
Sergio <sergiohidalgocaceres@gmail.com>
2021-09-05 19:06:24 -0500

    cura

--- 02-Mounstro/assets/scripts/app.js
+++ 02-Mounstro/assets/scripts/app.js
@@ -1,6 +1,7 @@
 const ATTACK_VALUE = 14;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 15;
+const HEALTH_VALUE = 20;
 
 const choseMaxLife = 100;
 let currentMonsterHealth = choseMaxLife;
@@ -8,16 +9,7 @@ let currentPlayerHealth = choseMaxLife;
 
 adjustHealthBars(choseMaxLife);
 
-function attackMonsterHandler(mode) {
-  let maxDamage;
-  if (mode === 'ATTACK') {
-    maxDamage = ATTACK_VALUE;
-  } else if (mode === 'STRONG_ATTACK') {
-    maxDamage = STRONG_ATTACK_VALUE;
-  }
-
-  const damage = dealMonsterDamage(maxDamage);
-  currentMonsterHealth -= damage;
+function endRound() {
   const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
   currentPlayerHealth -= playerDamage;
 
@@ -30,6 +22,19 @@ function attackMonsterHandler(mode) {
   }
 }
 
+function attackMonsterHandler(mode) {
+  let maxDamage;
+  if (mode === 'ATTACK') {
+    maxDamage = ATTACK_VALUE;
+  } else if (mode === 'STRONG_ATTACK') {
+    maxDamage = STRONG_ATTACK_VALUE;
+  }
+
+  const damage = dealMonsterDamage(maxDamage);
+  currentMonsterHealth -= damage;
+  endRound();
+}
+
 function attackHandler() {
   attackMonsterHandler('ATTACK');
 }
@@ -38,5 +43,24 @@ function strongAttackHandler() {
   attackMonsterHandler('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+  if (currentPlayerHealth <= 0 || currentMonsterHealth <= 0) {
+    return;
+  }
+
+  let healthValue;
+  if (currentPlayerHealth >= choseMaxLife - HEALTH_VALUE) {
+    alert('No puedes tener más cura que el máximo de salud');
+    healthValue = choseMaxLife - currentPlayerHealth;
+  } else {
+    healthValue = HEALTH_VALUE;
+  }
+
+  currentPlayerHealth += healthValue;
+  increasePlayerHealth(healthValue);
+  endRound();
+}
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
