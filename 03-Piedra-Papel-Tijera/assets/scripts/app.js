c83b8824790aed25a643474f37ba4dc5a473f056
Sergio <sergiohidalgocaceres@gmail.com>
2021-09-12 19:36:48 -0500

    más funciones

---
+++ 03-Piedra-Papel-Tijera/assets/scripts/app.js
@@ -0,0 +1,14 @@
+function startGame() {
+  console.log('Game is starting...');
+}
+
+const game = {
+  title: 'Juego Piedra, Papel y Tijera',
+  start: function start() {
+    console.log('Game is starting...');
+  },
+};
+
+startBtn.addEventListener('click', game.start);
+
+//startGame();
