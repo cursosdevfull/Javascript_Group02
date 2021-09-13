d6fb6c7c09df42c10e5132b3913bdafce20efe62
Sergio <sergiohidalgocaceres@gmail.com>
2021-09-12 19:22:06 -0500

    use strict

--- Conocimientos/01-ambitos/app.js
+++ Conocimientos/01-ambitos/app.js
@@ -1,10 +1,27 @@
+'use strict';
+const age = 10;
+
+info = { username: 'user1' };
+console.log(info);
+
+console.log('age', age);
+
+function printInfoUser(gender, gender) {
+  nombre = 'Milagros';
+  age = 12;
+
+  console.log('nombre', nombre, 'age', age);
+}
+
+printInfoUser();
+
 console.log(username);
 var username = 'sergio.hidalgo';
 
 console.log(username);
 
-console.log(password);
-let password = '123'; // SOLO FUNCIONA CON VAR
+//console.log(password);
+//let password = '123'; // SOLO FUNCIONA CON VAR
 
 /* const nombre = 'Andrea';
 
