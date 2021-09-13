345345ba8e9a18b5bc159a23d1f0292f3166bbdc
Sergio <sergiohidalgocaceres@gmail.com>
2021-09-12 19:00:12 -0500

    ambitos de variables

---
+++ Conocimientos/01-ambitos/app.js
@@ -0,0 +1,24 @@
+/* const nombre = 'Andrea';
+
+function printInfoUser() {
+  let nombre = 'Milagros';
+  let age = 12;
+
+  console.log('nombre', nombre, 'age', age);
+}
+
+printInfoUser();
+
+console.log('nombre', nombre); */
+//console.dir(window);
+//console.log('name', name);
+
+const name = 'Sergio';
+let hobbies;
+
+if (name === 'Sergio') {
+  hobbies = ['enseñar', 'cocinar', 'reuniones'];
+  console.log('hobbies', hobbies);
+}
+
+console.log('hobbies', hobbies);
